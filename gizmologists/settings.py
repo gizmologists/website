26596cfb057884a42442199875a5c5b23c0b0bb4
Evan Typanski <evantypanski@gmail.com>
2018-06-24 12:31:57 -0400

    Update favicon

--- gizmologists/settings.py
+++ gizmologists/settings.py
@@ -84,39 +84,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'gizmologists.wsgi.application'
 
-LOGGING = {
-    'version': 1,
-    'disable_existing_loggers': False,
-    'formatters': {
-        'verbose': {
-            'format' : "[%(asctime)s] %(levelname)s [%(name)s:%(lineno)s] %(message)s",
-            'datefmt' : "%d/%b/%Y %H:%M:%S"
-        },
-        'simple': {
-            'format': '%(levelname)s %(message)s'
-        },
-    },
-    'handlers': {
-        'file': {
-            'level': 'DEBUG',
-            'class': 'logging.FileHandler',
-            'filename': 'mysite.log',
-            'formatter': 'verbose'
-        },
-    },
-    'loggers': {
-        'django': {
-            'handlers':['file'],
-            'propagate': True,
-            'level':'DEBUG',
-        },
-        'MYAPP': {
-            'handlers': ['file'],
-            'level': 'DEBUG',
-        },
-    }
-}
-
 # Database
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
 
