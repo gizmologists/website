42b6e8c0ab7113e7dbf5f45d550eb3ec9982c686
Evan Typanski <evantypanski@gmail.com>
2018-07-15 14:52:51 -0400

    Split development and production settings

---
+++ gizmologists/settings_development.py
@@ -0,0 +1,7 @@
+SECRET_KEY = 'ly$4-u^+7x$*)!j9&j!9=kzf4x8x9u=nopcmsxto+nw9t6mewl'
+DEBUG = True
+
+ALLOWED_HOSTS = []
+
+STATIC_URL = '/static/'
+MEDIA_URL = '/media/'
---
+++ gizmologists/settings_production.py
@@ -0,0 +1,24 @@
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
+DEBUG = False
+
+ALLOWED_HOSTS = [
+  "gizmologists.herokuapp.com"
+]
+
+# AWS
+AWS_ACCESS_KEY_ID = os.environ.get('GIZMOLOGISTS_AWS_ACCESS_KEY_ID')
+AWS_SECRET_ACCESS_KEY = os.environ.get('GIZMOLOGISTS_AWS_SECRET_ACCESS_KEY')
+AWS_STORAGE_BUCKET_NAME = 'gizmologists'
+
+# Tell django-storages the domain to use to refer to static files.
+AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
+
+# STATIC FILES
+STATICFILES_STORAGE = 'custom_storages.StaticStorage'
+MEDIAFILES_STORAGE = 'custom_storages.MediaStorage'
+
+STATIC_URL = "https://%s/%s/" % (AWS_S3_CUSTOM_DOMAIN, STATICFILES_LOCATION)
+MEDIA_URL = "https://%s/%s/" % (AWS_S3_CUSTOM_DOMAIN, MEDIAFILES_LOCATION)
+
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+AWS_QUERYSTRING_AUTH = False
